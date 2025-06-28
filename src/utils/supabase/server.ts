0a0381ba28a396879f5fb128d1b0667923933dee
USBABC1 <contatousbabc@gmail.com>
2025-06-28 15:40:41 -0300

    Update server.ts

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,4 +3,33 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieSt
+  const cookieStore = cookies()
+
+  // Cria um cliente Supabase para o lado do servidor.
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            // A ação `set` pode falhar em Server Actions ou Route Handlers.
+            // Isso não é um problema, pois o middleware se encarregará de definir o cookie.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {
+            // Ação `remove` pode falhar em Server Actions ou Route Handlers.
+          }
+        },
+      },
+    }
+  )
+}
