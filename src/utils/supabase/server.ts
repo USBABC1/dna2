54bea4a27b272ceed157bcb7e552e506b59e2a77
USBABC1 <contatousbabc@gmail.com>
2025-06-28 15:19:00 -0300

    Create server.ts

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,6 @@
+// src/utils/supabase/server.ts
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export function createClient() {
+  const cookieSt
